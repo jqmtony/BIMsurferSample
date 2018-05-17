235169a19a71682bde1622473a105be934143f8f
jqmtony <jqmtony@NB-E440.lan>
2018-05-18 01:55:07 +0800

    update

--- BIMsurferViewerV1/src/main/java/org/xplus/bim/controller/BimSurferViewerController.java
+++ BIMsurferViewerV1/src/main/java/org/xplus/bim/controller/BimSurferViewerController.java
@@ -1,5 +1,13 @@
 package org.xplus.bim.controller;
 
+import org.springframework.stereotype.Controller;
+
+/**
+ * BimSurfer功能控制器.
+ * @author jqmtony
+ *
+ */
+@Controller
 public class BimSurferViewerController {
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# BIMsurferSample
+BIMsurfer Sample
+================
+
+本项使用BIMsurfer插件对BIMServer进行远程调用。
